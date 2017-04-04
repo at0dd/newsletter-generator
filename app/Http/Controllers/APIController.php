99e02462cef1a59a9dcd46035f5907245d1897c8
at0dd <atodd@atodd.io>
2017-04-04 09:15:53 -0500

    API Auth

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -13,54 +13,89 @@ class APIController extends Controller
   {
     $articles = Article::with('categories')->where('approved', 1)->where('archived', 0)->get();
     return response()->json(['articles' => $articles], 200);
-    //return Response::json($articles, 200);
   }
 
   public function Approve($id)
   {
-    $article = Article::where('id', $id)->first();
-    $article->approved = true;
-    $article->save();
-    return Response::json(200);
+    if(Auth::check()){
+      if(Auth::user()->hasRole('Administrator'))
+      {
+        $article = Article::where('id', $id)->first();
+        $article->approved = true;
+        $article->save();
+        return Response::json(200);
+      }
+      return Response::json(401);
+    }
+    return Response::json(403);
   }
 
   public function Deny($id)
   {
-    $article = Article::where('id', $id)->first();
-    $article->approved = false;
-    $article->save();
-    return Response::json(200);
+    if(Auth::check()){
+      if(Auth::user()->hasRole('Administrator'))
+      {
+        $article = Article::where('id', $id)->first();
+        $article->approved = false;
+        $article->save();
+        return Response::json(200);
+      }
+      return Response::json(401);
+    }
+    return Response::json(403);
   }
 
   public function Role($id, $role)
   {
-    $user = User::where('id', $id)->first();
-    $roleo = Role::where('name', $role)->first();
-    if($user->hasRole($role)){
-      $user->roles()->detach($roleo->id);
-    } else {
-      $user->roles()->attach($roleo->id);
+    if(Auth::check()){
+      if(Auth::user()->hasRole('Administrator'))
+      {
+        $user = User::where('id', $id)->first();
+        $roleo = Role::where('name', $role)->first();
+        if($user->hasRole($role)){
+          $user->roles()->detach($roleo->id);
+        } else {
+          $user->roles()->attach($roleo->id);
+        }
+        $user->save();
+        return Response::json(200);
+      }
+      return Response::json(401);
     }
-    $user->save();
-    return Response::json(200);
+    return Response::json(403);
   }
 
   public function Archive($id)
   {
-    $article = Article::where('id', $id)->first();
-    if($article->archived){
-      $article->archived = 0;
-    } else {
-      $article->archived = 1;
+    if(Auth::check()){
+      if(Auth::user()->hasRole('Administrator'))
+      {
+        $article = Article::where('id', $id)->first();
+        if($article->archived){
+          $article->archived = 0;
+        } else {
+          $article->archived = 1;
+        }
+        $article->save();
+        return Response::json(200);
+      }
+      return Response::json(401);
     }
-    $article->save();
-    return Response::json(200);
+    return Response::json(403);
   }
 
   public function SendNewsletter()
   {
-    $articles = Article::with('categories')->where('approved', 1)->where('archived', 0)->get();
-    Mail::to('atodd@ksu.edu')->send(new Newsletter($articles));
-    return Response::json(200);
+    if(Auth::check()){
+      if(Auth::user()->hasRole('Administrator'))
+      {
+        $articles = Article::with('categories')->where('approved', 1)->where('archived', 0)->get();
+        Mail::to('atodd@ksu.edu')->send(new Newsletter($articles));
+        return Response::json(200);
+      }
+      return Response::json(401);
+    }
+    return Response::json(403);
+
   }
 }
